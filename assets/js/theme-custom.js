8171ecb615e36a1fffc140546356041820c8ced8
Tobi <delesafatobi@gmail.com>
2020-05-12 20:49:28 +0100

    Update theme-custom.js

--- assets/js/theme-custom.js
+++ assets/js/theme-custom.js
@@ -1,1 +1,15 @@
 // File for your custom JavaScript
+
+$(function() {
+    var check = $('#filterControls .btnn');
+    check.on('click', function() {
+        setTimeout(function() {
+            $(".filters div").show();
+            var choice = $.map(check.filter('.active'), function(el) {
+                return "." + el.id;
+            }).join("");
+            if (choice)
+                $(".filters div").not(choice).hide();
+        }, 1);
+    });
+});
